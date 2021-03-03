8064e63efcbe6a372bffd1428874f4eeb6a242f4
Lucas-schimit <lucas_schimit@outlook.com>
2021-03-03 17:44:28 -0300

    Subindo a Spring para o GitHub

---
+++ Spring-TDD/src/test/java/com/Spring/TDD/Spring/TDD/SpringTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.TDD.Spring.TDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
